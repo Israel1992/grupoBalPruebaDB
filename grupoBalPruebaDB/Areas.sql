4770fd81ac7969b951ca3b13ab640167fba7c8ee
israel1992 <edgar.israel1992@gmil.com>
2023-11-06 11:26:47 -0600

    se sube proyecto

---
+++ grupoBalPruebaDB/Areas.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Areas]
+(
+	[Id_area] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [descripcion] VARCHAR(80) NULL
+)
---
+++ grupoBalPruebaDB/Empleados.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Empleados]
+(
+	[Id_empleado] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [nombre] VARCHAR(20) NULL, 
+    [correo] VARCHAR(40) NULL, 
+    [telefono] VARCHAR(20) NULL, 
+    [id_area] INT NOT NULL, 
+    CONSTRAINT [FK_Empleados_ToAreas] FOREIGN KEY ([id_area]) REFERENCES [Areas]([Id_area])
+)
